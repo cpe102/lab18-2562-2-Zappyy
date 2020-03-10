e7a1373a3c2c9d11a57ae48b4a646902073dc68f
lab302-32 <lab302-32@labeng.local>
2020-03-10 17:16:09 +0700

    1

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -3,15 +3,25 @@ using namespace std;
 
 struct Rect{
 	double x,y,w,h;
+	
 };
 
-double overlap(Rect,Rect);
+double overlap(Rect ,Rect);
 
 int main(){
+    Rect a;
+    Rect b;
 	cout << "Please input Rect 1 (x y w h): ";
+	cin >> a.x >> a.y >> a.w >> a.h;
 	cout << "Please input Rect 2 (x y w h): ";
+    cin >> b.x >> b.y >> b.w >> b.h;
 	
-	
-	cout << "Overlap area = ";	
+	cout << "Overlap area = " << overlap(a,b);
 	return 0;
+}
+double overlap(Rect a,Rect b){
+    double areaA = abs(x.a-y.b)*abs(w.a-h.b);
+	double areaB = abs(x.b-y.a)*abs(w.b-h.a);
+	double areaI = (min(x.a,y.a)-(max(w.a,h.a))*(min(x.b,y.b)-(max(w.b,w.h)));
+     
 }
